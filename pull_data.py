f1c2fdc4202b6d6fd7ee5bc0b41a511d91d70d19
jackedison <jack.edison95@gmail.com>
2020-10-17 23:17:40 +0200

    adjusted to pull weekly data

--- pull_data.py
+++ pull_data.py
@@ -3,25 +3,28 @@ import pandas as pd
 from pytrends.request import TrendReq
 from datetime import datetime
 
-def pull_data_to_csv(keyword):
+def pull_data_to_csv(keyword, start, end, geo, cat, gprop, method, verbose):
     pytrend = TrendReq(hl='en-US')
-    keyword = keyword
-    start = '2019-01-01'
-    end = '2020-10-15'
-    geo = 'US'
-    cat = 0
-    gprop = ''
 
-    overlapping = pytrend_daily.get_daily_trend(pytrend, keyword, start, end, geo=geo, cat=cat, gprop=gprop, verbose=True, tz=0)
+    # Overlapping (ensure more than 270 days)
+    overlapping = pytrend_daily.get_daily_trend(pytrend, keyword, start, end,
+                                                method=method, geo=geo, cat=cat,
+                                                gprop=gprop, verbose=verbose)
 
+    # Weekly data
+    kw_list = [keyword]
+    tf = start+' '+end
+
+    weekly = pytrend_daily.fetch_data(pytrend, kw_list, timeframe=tf,)
+
+    # Daily range to compare (ensure less than 270 days)
     start = '2020-03-01'
     end = '2020-10-15'
-    kw_list = [keyword]
-    start_d = datetime.strptime(start, '%Y-%m-%d')
-    end_d = datetime.strptime(end, '%Y-%m-%d')
-    tf = start_d.strftime('%Y-%m-%d')+' '+end_d.strftime('%Y-%m-%d')
+    tf = start+' '+end
 
     daily = pytrend_daily.fetch_data(pytrend, kw_list, timeframe=tf,)
 
+    # Save as csvs
     overlapping.to_csv('data/{}_overlapping.csv'.format(keyword))
+    weekly.to_csv('data/{}_weekly.csv'.format(keyword))
     daily.to_csv('data/{}_daily.csv'.format(keyword))
